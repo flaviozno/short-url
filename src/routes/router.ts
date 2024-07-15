dc0f5b12eaf609cedd9bcb7d256a503d01cff469
flaviozno <flavio.vezono@ufu.br>
2024-07-15 09:33:01 -0300

    health

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -4,9 +4,10 @@ import urlController from "../controllers/urlController";
 const router = Router();
 
 router.post('/shorten-url', urlController.createUrl)
-router.get('/*', urlController.getFullUrl)
 router.get('/health', (req, res) => {
     res.status(200).send('OK')
 })
+router.get('/*', urlController.getFullUrl)
+
 
 export default router;
